bfefd61a0acf206fe60d41159ab82c94fa627363
Kishore Kumar P <kish@Kishores-MacBook-Pro.local>
2021-11-28 22:15:41 -0500

    replaced placeholder URLs with loadbalancer URLs from GCP

--- terminalApp/src/index.js
+++ terminalApp/src/index.js
@@ -1,10 +1,10 @@
 
 let urls = 
 {
-    "HADOOP" : "https:\/\/www.youtube.com",
-    "SPARK" : "https:\/\/www.google.com",
-    "JUPYTER":"https:\/\/www.facebook.com",
-    "SONAR":"https:\/\/www.apple.com"
+    "HADOOP" : "http:\/\/35.223.128.64:8088/",
+    "SPARK" : "http:\/\/34.132.88.138:8080",
+    "JUPYTER":"http:\/\/35.184.234.139:8888",
+    "SONAR":"http:\/\/34.136.255.137:9000"
 
 }
 
